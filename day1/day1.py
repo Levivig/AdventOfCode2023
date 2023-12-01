cf9cfb4a1c660669f6c1931b5f3b7db400956882
Levente Vig <levente.vig@icloud.com>
2023-12-01 07:38:53 +0100

    ⭐️⭐️ day 1 solved

--- day1/day1.py
+++ day1/day1.py
@@ -12,14 +12,65 @@ inputfile = os.path.join(dirname, 'input.txt')
 
 def part1(data):
     print('part1:')
+    total = 0
     for line in data:
-        print(line)
+        total += solve(line)
+
+    print(total)
 
 
 def part2(data):
     print('part2:')
+    total = 0
     for line in data:
-        print(line)
+        line = find_and_combine_first_last_numbers(line)
+        total += solve(line)
+
+    print(total)
+
+
+def solve(line):
+    numbers = []
+    for c in line:
+        if c.isnumeric():
+            numbers.append(int(c))
+
+    if numbers:
+        res = int("{}{}".format(numbers[0], numbers[-1]))
+        return res
+    return 0
+
+
+def find_and_combine_first_last_numbers(s):
+    num_words = {
+        'one': '1', 'two': '2', 'three': '3', 'four': '4', 'five': '5',
+        'six': '6', 'seven': '7', 'eight': '8', 'nine': '9'
+    }
+
+    first_number = None
+    last_number = None
+
+    # Find the first number or number word
+    for i in range(len(s)):
+        for word, digit in num_words.items():
+            if s[i:i+len(word)] == word:
+                first_number = digit
+                break
+        if first_number or s[i].isdigit():
+            first_number = first_number if first_number else s[i]
+            break
+
+    # Find the last number or number word
+    for i in range(len(s) - 1, -1, -1):
+        for word, digit in num_words.items():
+            if i - len(word) + 1 >= 0 and s[i-len(word)+1:i+1] == word:
+                last_number = digit
+                break
+        if last_number or s[i].isdigit():
+            last_number = last_number if last_number else s[i]
+            break
+
+    return first_number + last_number if first_number and last_number else None
 
 
 def main():
@@ -27,8 +78,8 @@ def main():
     print(currentDay)
     with open(inputfile) as f:
         data = f.read().splitlines()
-        part1(data)
-        part2(data)
+        part1(data) # 53194
+        part2(data) # 54249
 
 
 if __name__ == "__main__":
